cdb75ee984c26cbfb25b512ac5d7d603df486808
christian <christianjones94@gmail.com>
2022-12-06 20:44:25 -0500

    tailwind test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1 className="text-3xl font bold underline">Good Sport Golf Club</h1>
+    <div className="app">
+      <div>
+        <Navbar />
+      </div>
+      <div className="">
+        <img
+          src="https://images.unsplash.com/photo-1597369237991-5c95d1b6e0c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Z29sZnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
+          alt=""
+          className=".w-5/6 "
+        />
+      </div>
     </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      blue: "#7BC4F2",
+      "gray-dark": "#273444",
+      gray: "#8492a6",
+      "gray-light": "#d3dce6",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
     extend: {},
   },
   plugins: [],
