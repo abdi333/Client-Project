51b98d1c9e3e3cafb9032f095c0ac943e9e6c7e2
Abdi Hirsi <abdi.omer33@gmail.com>
2022-12-09 18:35:43 -0500

    saving to show progress updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import 'tailwindcss/tailwind.css';
 import Nav from "./components/Join/Nav";
 import JoinLeague from "./components/Join/JoinLeague";
+import Footer from './components/Join/Footer';
 
 
 
 export default function App() {
   return (
     <div className="app">
-      <div className="" >
+      <div className="style={ImgStyle}" >
         <Nav />        
         <JoinLeague />
+        <Footer />
       </div>      
     </div>
   );
 }
 
-// style={ImgStyle}
-// const ImgStyle = {
-//   width: '100%',
-//   height: '1200px',
-//   backgroundImage: `url("https://images.unsplash.com/photo-1632946269126-0f8edbe8b068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80")`,
-//   backgroundSize: 'cover'
-// };
+
+const ImgStyle = {
+  width: '100%',
+  height: '1200px',
+  backgroundImage: `url("https://images.unsplash.com/photo-1632946269126-0f8edbe8b068?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1131&q=80")`,
+  backgroundSize: 'cover'
+};
 
--- src/components/Join/JoinLeague.jsx
+++ src/components/Join/JoinLeague.jsx
@@ -16,39 +16,39 @@ export default function JoinLeague() {
     const [email, setEmail] = useState("");
     const emailEl = useRef();
 
-    const [password, setPassword] = useState("");
-    const passwordEl = useRef();
-    
-    const [pwd, setPwd] = useState('');
-    const [isRevealPwd, setIsRevealPwd] = useState(false);
-    const [valid, setValid] = useState(false);
+    const [key, setKey] = useState("");
+    const keyEl = useRef();
 
-    useEffect (() => {
-        if (password.length < 8) {
+    const [valid, setValid] = useState(false);
+    const [isRevealPwd, setIsRevealPwd] = useState(false);
+
+
+    useEffect(() => {
+        if (key.length < 8) {
             setValid(true);
         } else {
             setValid(false);
         }
-    }, [ password ]);
+    }, [key]);
 
     return (
         <div className="p-8 lg:w-1/2 mx-auto">
-            <div className="bg-gray-100 rounded-t-lg p-8">
+            <div className=" rounded-t-lg p-8">
                 <p className="text-center text-md">Sign up with</p>
                 <div>
                     <div className="flex items-center justify-center space-x-5 mt-3">
                         <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                            <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" className="w-6 h-6 mr-3" viewBox="0 0 48 48"/>
+                            <img src="https://cdn-icons-png.flaticon.com/512/281/281764.png" className="w-6 h-6 mr-3" viewBox="0 0 48 48" />
                             Google
                         </button>
-                         <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-                            <img src="https://cdn-icons-png.flaticon.com/512/2504/2504884.png" viewBox="0 0 16 16" className="w-6 h-6 mr-3"/>
+                        <button className="flex items-center py-2 px-4 text-sm uppercase rounded bg-white hover:bg-gray-100 text-indigo-500 border border-transparent hover:border-transparent hover:text-gray-700 shadow-md hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+                            <img src="https://cdn-icons-png.flaticon.com/512/2504/2504884.png" viewBox="0 0 16 16" className="w-6 h-6 mr-3" />
                             Apple
                         </button>
                     </div>
                 </div>
             </div>
-            <div className="bg-gray-100 rounded-b-lg py-12 px-4 lg:px-24">
+            <div className="rounded-b-lg py-12 px-4 lg:px-24">
                 <p className="text-center text-md">Or sign up with credentials</p>
                 <form className="mt-6">
                     <div className="relative">
@@ -72,12 +72,12 @@ export default function JoinLeague() {
                     <div className="relative mt-3">
                         <input className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline" type="text" placeholder="Phone Number" ref={phoneEl} />
                         <div className="absolute left-0 inset-y-0 flex items-center">
-                            <img src="https://cdn-icons-png.flaticon.com/512/597/597177.png" className="h-7 w-7 ml-3 text-gray-400 p-1" viewBox="0 0 20 20" fill="currentColor" />
+                            <img src="https://cdn-icons-png.flaticon.com/128/159/159832.png" className="h-7 w-7 ml-3 text-gray-400 p-1" viewBox="0 0 20 20" fill="currentColor" />
                         </div>
                     </div>
                     <div className="relative mt-3">
                         <input className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
-                             type={isRevealPwd ? "text" : "password"}  placeholder="Password" value={pwd}  onChange={(e) => setPwd(e.target.value)} />
+                            type={isRevealPwd ? "text" : "password"} placeholder="Password" ref={keyEl} onChange={(e) => setKey(e.target.value)} />
                         <div className="absolute left-0 inset-y-0 flex items-center justify-between">
                             <img src="https://cdn-icons-png.flaticon.com/512/25/25215.png" className="h-7 w-7 ml-3 text-gray-400 p-1" viewBox="0 0 20 20" fill="currentColor" />
                             <img className="w-1/12 ml-48"
@@ -86,33 +86,30 @@ export default function JoinLeague() {
                                 onClick={() => setIsRevealPwd(prevState => !prevState)} />
                         </div>
                     </div>
-                    <div className="relative mt-3">
-                        <input className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline" type="text" placeholder="Confirm Password" ref={passwordEl} value={password} onChange={(e) => setPassword(e.target.value)}/>
-                        <div className="absolute left-0 inset-y-0 flex items-center">
-                            <img src="https://cdn-icons-png.flaticon.com/512/25/25215.png" className="h-7 w-7 ml-3 text-gray-400 p-1" viewBox="0 0 20 20" fill="currentColor" />
-                        </div>
-                    </div>
                     <p className="mt-4 italic text-black font-light text-sm">Password should be greater than 8 characters</p>
                     <p className="mt-4 italic text-black font-light text-sm">Password strength: <span style={{ color: valid ? 'red' : 'green' }}>{valid ? 'Weak' : 'Strong'}</span></p>
 
                     <div className="mt-4 flex items-center text-black"> <input type="checkbox" id="remember" name="remember" className="mr-2" />
-                        <label className="text-sm" />I agree with the <a className="text-indigo-400 hover:text-indigo-500" href="#">Privacy Policy</a>
+                        <label className="text-sm" />I agree with the<span>&nbsp;</span><a className="text-blue hover:text-indigo-600" href="#">Privacy Policy</a>
                     </div>
 
                     <div className="flex items-center justify-center mt-8">
-                        <button className=" border text-black py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
-                            onClick={function () { setFirst(firstEl.current.value); setLast(lastEl.current.value); setEmail(emailEl.current.value); setPhone(phoneEl.current.value); setPwd(passwordEl.current.value) }}>Sign Up</button>
+                        <button className=" text-black py-2 px-4 uppercase rounded bg-blue hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5"
+                            onClick={function () {
+                                setFirst(firstEl.current.value); setLast(lastEl.current.value); setEmail(emailEl.current.value); setPhone(phoneEl.current.value); setKey(keyEl.current.value);
+                                firstEl.current.value = ""; lastEl.current.value = ""; emailEl.current.value = ""; phoneEl.current.value = ""; keyEl.current.value = "";
+                            }}>Sign Up</button>
                     </div>
                 </form>
             </div>
             <div>
-                <h4 className="text-black">                
+                {/* <h4 className="text-black">
                     First Name : {first} <br /><br />
                     Last Name : {last} <br /><br />
                     Phone Number : {phone} <br /><br />
                     Email Adress : {email} <br /><br />
-                    Password : {password}
-                </h4>
+                    Password : {key}
+                </h4> */}
             </div>
         </div>
 
