b22a582d8b2fbab694d8ed3e119b6751e02063db
Abdi Hirsi <abdi.omer33@gmail.com>
2022-12-09 22:45:26 -0500

    moved landing page component to main branch

--- src/components/App/Testimonials.jsx
+++ src/components/App/Testimonials.jsx
@@ -7,7 +7,7 @@ export default function Testimonials() {
                 <div className="grid gap-8 md:grid-rows-2 lg:grid-cols-2">
 
 
-                    <div className="border rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
+                    <div className=" rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
                         <img className="w-20 h-20 mx-auto rounded-full" src="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp" alt="user avatar" height="120" width="220" loading="lazy" />
                         <div className="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
                             <p className="text-black text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
@@ -17,17 +17,17 @@ export default function Testimonials() {
                             </div>
                         </div>
                     </div>
-                    <div className="border border-gray rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
+                    <div className=" rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
                         <img className="w-20 h-20 mx-auto rounded-full" src="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/third_user.webp" alt="user avatar" height="220" width="220" loading="lazy" />
                         <div className="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
-                            <p className="text-white text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
+                            <p className="text-black text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
                             <div>
-                                <h6 className="text-white text-lg font-semibold leading-none">John Doe</h6>
-                                <span className="text-xs text-white">Product Designer</span>
+                                <h6 className="text-black text-lg font-semibold leading-none">John Doe</h6>
+                                <span className="text-xs text-black">Product Designer</span>
                             </div>
                         </div>
                     </div>
-                    <div className="border-nonerounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
+                    <div className=" rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
                         <img className="w-20 h-20 mx-auto rounded-full" src="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/first_user.webp" alt="user avatar" height="120" width="220" loading="lazy" />
                         <div className="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
                             <p className="text-black text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
@@ -37,16 +37,17 @@ export default function Testimonials() {
                             </div>
                         </div>
                     </div>
-                    <div className="border border-gray rounded-xl bg-blue sm:flex sm:space-x-8 sm:p-2">
+                    <div className=" rounded-xl bg-gray-50 sm:flex sm:space-x-8 sm:p-2">
                         <img className="w-20 h-20 mx-auto rounded-full" src="https://tailus.io/sources/blocks/grid-cards/preview/images/avatars/third_user.webp" alt="user avatar" height="220" width="220" loading="lazy" />
                         <div className="space-y-4 mt-4 text-center sm:mt-0 sm:text-left">
-                            <p className="text-white text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
+                            <p className="text-black text-md"> <span className="font-serif">"</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat repellat perspiciatis excepturi est. Non ipsum iusto aliquam consequatur repellat provident, omnis ut, sapiente voluptates veritatis cum deleniti repudiandae ad doloribus. <span className="font-serif">"</span></p>
                             <div>
-                                <h6 className="text-white text-lg font-semibold leading-none">John Doe</h6>
-                                <span className="text-xs text-white">Product Designer</span>
+                                <h6 className="text-black text-lg font-semibold leading-none">John Doe</h6>
+                                <span className="text-xs text-black">Product Designer</span>
                             </div>
                         </div>
                     </div>
+                    
                 </div>
             </div>
         </div>
